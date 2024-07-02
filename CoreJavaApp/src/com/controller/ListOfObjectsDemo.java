fad4497c4c89e9699b8e01d094651006f44285fd
imtiyaz-hirani <i.hirani@techskillsit.com>
2024-07-03 01:22:57 +0530

    List with Comparable and COmparator

---
+++ CoreJavaApp/src/com/controller/ListOfObjectsDemo.java
@@ -0,0 +1,78 @@
+package com.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.model.Employee;
+
+public class ListOfObjectsDemo {
+
+	public static void main(String[] args) {
+		List<Employee> list = new ArrayList<>(); 
+		Employee employee1 = new Employee(2,"harry potter","IT","london",88000D);
+		Employee employee2 = new Employee(4,"ronald weasley","ADMIN","surrey",75000D);
+		Employee employee3 = new Employee(1,"hermione granger","DEV","london",95000D);
+		
+		list = Arrays.asList(employee1,employee2,employee3);
+		
+		//iteration 
+		for(Employee e : list) {
+			System.out.println(e);
+		}
+		
+		//sorting 
+		Collections.sort(list); //e1.compareTo(e2)
+		
+		System.out.println("*********Sorted List by Salary : ASC*******");
+		for(Employee e : list) {
+			System.out.println(e);
+		}
+		
+		
+		/* Sorting using Comparator interface */
+		System.out.println("*********Sorted List by Salary : ASC*******");
+		Collections.sort(list, new A());
+		for(Employee e : list) {
+			System.out.println(e);
+		}
+		System.out.println("*********Sorted List by Salary : DESC*******");
+		Collections.sort(list, new B());
+		for(Employee e : list) {
+			System.out.println(e);
+		}
+		System.out.println("*********Sorted List by CITY : ASC*******");
+		Collections.sort(list, new C());
+		for(Employee e : list) {
+			System.out.println(e);
+		}
+	}
+}
+
+class A implements Comparator<Employee>{
+
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		 
+		return (int)(e1.getSalary() - e2.getSalary());
+	}
+	
+}
+
+class B implements Comparator<Employee>{
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		 
+		return (int)(e2.getSalary() - e1.getSalary());
+	}
+}
+
+class C implements Comparator<Employee>{
+	@Override
+	public int compare(Employee e1, Employee e2) {
+		
+		return e1.getCity().compareTo(e1.getCity()) ;
+	}
+}
--- CoreJavaApp/src/com/model/Employee.java
+++ CoreJavaApp/src/com/model/Employee.java
@@ -1,12 +1,30 @@
 package com.model;
 
-public class Employee extends Object{
+public class Employee extends Object implements Comparable<Employee>{
 	/* instance variables */
     private int id; 
     private String name; 
     private String department;
     private String city;
+	private double salary; 
+
+	public Employee(int id, String name, String department, String city, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+		this.city = city;
+		this.salary = salary;
+	}
 	
+	
+
+	public Employee() {
+		 
+	}
+
+
+
 	public int getId() {
 		return id;
 	}
@@ -37,10 +55,34 @@ public class Employee extends Object{
 
 	public void setCity(String city) {
 		this.city = city;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", department=" + department + ", city=" + city + ", salary="
+				+ salary + "]";
+	}
+
+	@Override
+	public int compareTo(Employee e2) {
+		 return (int) (this.salary - e2.salary);
 	} 
-	 
+	 //ASC - DESC:---ASC---- 
+	//Employee - salary 
+	//e1: 10000  e2: 12000 
+	//[e1,e2] : [e1,e2]      e1.salary < e2.salary : no change :-ve
 	
-	
-	
-}
+}//e1: 15000  e2: 12000 
+//[e1,e2] : [e2,e1]      e1.salary > e2.salary : swap : +ve 
+//e1: 12000  e2: 12000 
+//[e1,e2] = [e1,e2] e1.salary == e2.salary : no change : 0 
+
  
